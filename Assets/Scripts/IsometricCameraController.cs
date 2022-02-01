632e6d42835c49bf6e723329cdbcc128201384ab
Sai Mangipudi <thegeekskm101@gmail.com>
2022-02-01 16:15:25 -0600

    Added Comments
    I added comments to make everything make sense.

--- Assets/Scripts/IsometricCameraController.cs
+++ Assets/Scripts/IsometricCameraController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class IsometricCameraController : MonoBehaviour
 {
+    /*This is probably the biggest class out of all the classes. This is just the camera movement controller for an isometric view.*/
+
     [Header("Camera Connections")]
     [SerializeField] Camera _mainCamera;
 
@@ -38,9 +40,15 @@ public class IsometricCameraController : MonoBehaviour
 
     void HandleInput()
     {
+        /*Do not look at this math. Or do and let me know how it works. I basically had no idea how to make motion work so I took a tutorial on isometric 
+         player movement and frankensteined the following unholy abomination from it to make it work for the camera. Bear in mind that this script is attached
+         to the CameraController empty and not the Camera itself.*/
+
         Vector3 rightMovement;
         Vector3 upMovement;
 
+
+        //I also added a sprint mechanic which i hate calling it a "mechanic" cuz it's literally just changing a float value.
         if (_enableSprintSpeed && Input.GetKey(KeyCode.LeftShift))
         {
             rightMovement = right * _cameraSprintSpeed * Time.deltaTime * Input.GetAxisRaw("Horizontal");
@@ -65,6 +73,8 @@ public class IsometricCameraController : MonoBehaviour
                 _mainCamera.orthographicSize = _maxZoomInValue;
             }
         }
+        /*So, this else state doesn't really need to be here, but in case some designer or somebody forgets to connect a camera to the serialized camera
+         variable in the beginning, this will just hook onto the default main camera.*/
         else
         {
             Camera.main.orthographicSize -= Input.GetAxis("Mouse ScrollWheel") * _cameraZoomSpeed;
@@ -78,6 +88,7 @@ public class IsometricCameraController : MonoBehaviour
             }
         }
 
+        //This is where movement gets calculated.
 
         transform.position += rightMovement;
         //Debug.Log(rightMovement);
@@ -87,7 +98,7 @@ public class IsometricCameraController : MonoBehaviour
 
     }
 
-
+    //Reeee
     private void Update()
     {
         HandleInput();
--- Assets/Scripts/Test_Crates.cs
+++ Assets/Scripts/Test_Crates.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class Test_Crates : MonoBehaviour, IInteractable
 {
+    /*This is a test class that I created to show case how an interactable would work. Obviously, there will probably be an interactable base 
+     class, but that class will still inherit the IInteractable Interface. For this, I kinda just skipped the interactable base class and just made
+     and interactable crate.*/
+
+    //This is not required. This is just to showcase visually how the IInteractable methods would work.
     [SerializeField] Rigidbody _rigidbody;
-    [SerializeField] MeshRenderer _meshRenderer;
 
-    void Awake()
-    {
-        
-    }
-
+    //These are just sample visual feedback methods.
+    #region VisualFeedback
     void MoveHorizontal()
     {
         Quaternion turnOffset = Quaternion.Euler(0, 30f, 0);
@@ -38,24 +39,29 @@ public class Test_Crates : MonoBehaviour, IInteractable
             _rigidbody.MoveRotation(_rigidbody.rotation * turnOffset);
         }
     }
+    #endregion
 
+    //This is when the mouse first hovers over the object.
     public void OnHoverEnter()
     {
         Debug.Log("Hovering over " + gameObject.name);
         MoveHorizontal();
     }
 
+    //This is when the mouse leaves the shape of the object.
     public void OnHoverExit()
     {
         Debug.Log("No Longer Hovering over" + gameObject.name);
     }
 
+    //This is when the mouse left clicks on the object.
     public void OnLeftClick()
     {
         Debug.Log("Left Clicked On" + gameObject.name);
         MoveVerticalForwards();
     }
 
+    //This is when the mouse right clicks on an object.
     public void OnRightClick()
     {
         Debug.Log("Right Clicked On" + gameObject.name);
